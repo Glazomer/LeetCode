039cb8015352ab2903dd5f94a5bb1fb03f89a0c8
Glazomer <dan2000kr@gmail.com>
2020-09-28 15:29:03 +0300

    still garbage, but better

--- 3Sum.py
+++ 3Sum.py
@@ -2,19 +2,18 @@ class Solution:
   def threeSum(self, nums: List[int]) -> List[List[int]]:
     result = set()
     c = Counter(nums)
-    nums = [[key] * (val if val < 3 else 3) for key, val in c.most_common()]
-    nums = [item for sublist in nums for item in sublist]
-    for i in range(len(nums)):
-      n1 = nums[i]
+    nums = []
+    for k, v in c.most_common():
+      nums.extend([k, k] if v > 1 else [k])
+      
+    for i, n1 in enumerate(nums):
       c[n1] -= 1
-      for j in range(i+1,len(nums)):
-        n2 = nums[j]
+      for n2 in nums[i+1:]:
         n3 = -(n1 + n2)
         c[n2] -= 1
         if n3 in c and c[n3] > 0:
-          numbs = [str(n1), str(n2), str(n3)]
-          numbs.sort()
-          result.add(','.join(numbs))
+          numbs = tuple(sorted([n1, n2, n3]))
+          result.add(numbs)
         c[n2] += 1
     print(nums)
-    return [map(lambda n: int(n), s.split(',')) for s in result]
+    return result
