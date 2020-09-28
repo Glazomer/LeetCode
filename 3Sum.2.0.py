7bb27929756686b73d8c4e6ee78a13bf153f99f0
Glazomer <dan2000kr@gmail.com>
2020-09-28 20:44:32 +0300

    another variant with sort in place

--- 3Sum.2.0.py
+++ 3Sum.2.0.py
@@ -1,24 +1,31 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
         result = set()
-        c = Counter(nums)
-        nums = [[k] * (v if v < 3 else 3) for k, v in c.most_common()]
-        nums = [n for subl in nums for n in subl]
         nums.sort()
 
-        _l = 0
-        _r = len(nums) - 1
+        num = nums[0] if nums else 0
+        inRow = 1
+        last = 0
+        for n in nums:
+            if not (num == n and inRow > 3):
+                nums[last] = n
+                last += 1
+                inRow = inRow + 1 if num == n else 1
+                num = n
+
+        nums = nums[:last]
+
         for i in range(len(nums)):
             t = -nums[i]  # target
-            l = _l
-            r = _r
+            l = 0
+            r = len(nums) - 1
             while l < r:
                 if l == i or nums[l] + nums[r] < t:
                     l += 1
                 elif r == i or nums[l] + nums[r] > t:
                     r -= 1
                 else:
-                    result.add(tuple(sorted([nums[l], nums[r], nums[i]])))
+                    result.add(tuple(sorted([nums[l], nums[r], -t])))
                     l += 1
                     r -= 1
 
