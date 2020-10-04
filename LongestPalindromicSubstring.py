8a2e980be3a1e7371a3a34badb64c87f14055094
Glazomer <dan2000kr@gmail.com>
2020-10-04 17:39:40 +0300

    made better naming and remove inline if logic

--- LongestPalindromicSubstring.py
+++ LongestPalindromicSubstring.py
@@ -1,6 +1,6 @@
 class Solution:
     def longestPalindrome(self, s: str) -> str:
-        L = R = l = r = 0
+        longestPalStart = longestPalEnd = l = r = 0
 
         while r < len(s):
             if l == r:
@@ -12,10 +12,12 @@ class Solution:
                         l -= 1
 
             if 0 <= l and s[l] == s[r]:
-                L, R = (l, r+1) if ((r+1)-l > R-L) else (L, R)
+                if ((r+1)-l > longestPalEnd-longestPalStart):
+                    longestPalStart = l
+                    longestPalEnd = r + 1
                 r += 1
                 l -= 1
             else:
-                l = r = (r + l) // 2 + 1  # going to midle +1 step
+                l = r = (r + l) // 2 + 1  # going to middle +1 step
 
-        return s[L:R]
+        return s[longestPalStart:longestPalEnd]
